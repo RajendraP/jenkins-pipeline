32255f3f167c013ea9acf51c914d6f4b1431a578
Rajendra Pandey <rupandey@gmail.com>
2018-09-27 16:13:38 +0530

    fixing path issues

--- vars/parseTestResultXML.groovy
+++ vars/parseTestResultXML.groovy
@@ -1,17 +1,38 @@
 def call(String testresultsfilePath){
     def jira_issues = []
-    def xml = new XmlParser().parse(testresultsfilePath)
-    xml.testcase.each{
-        test ->
-            def failedTests = [:]
-            if (test.failure){
-                failedTests.put('summary', test.@name)
-                failedTests.put('file', test.@file)
-                failedTests.put('details', test.failure.text())
-                failedTests.put('description', test.properties.property.'@value'[0].trim())
-                jira_issues << failedTests
-            }
+    println testresultsfilePath
+    try {
+        def xml = new XmlParser().parse(testresultsfilePath)
+        xml.testcase.each{
+            test ->
+                def failedTests = [:]
+                if (test.failure){
+                    failedTests.put('summary', test.@name)
+                    failedTests.put('file', test.@file)
+                    failedTests.put('details', test.failure.text())
+                    failedTests.put('description', test.properties.property.'@value'[0].trim())
+                    jira_issues << failedTests
+                }
+        }
+        jira_issues
     }
+    catch (FileNotFoundException exception){
+        println 'file not found'
+    }
+
+//    xml.testcase.each{
+//        test ->
+//            def failedTests = [:]
+//            if (test.failure){
+//                failedTests.put('summary', test.@name)
+//                failedTests.put('file', test.@file)
+//                failedTests.put('details', test.failure.text())
+//                failedTests.put('description', test.properties.property.'@value'[0].trim())
+//                jira_issues << failedTests
+//            }
+//    }
+//    jira_issues
+
     jira_issues
 }
 
