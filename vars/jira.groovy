fa646e2ffc9764573e8aa32b98a7fd42fc6fb2b9
Rajendra Pandey <rupandey@gmail.com>
2018-09-30 19:32:26 +0530

    adding jira id in TestResults.xml

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,36 +1,37 @@
 def call(String jiraprojectName, String jiraComponent, String resultsfilePath,
          String issueType='Bug', String fixVersions='pipeline_fixes') {
-    stage(name: 'Jira') {
-        withEnv(['JIRA_SITE=LOCAL']) {
+    parseTestResultXML resultsfilePath
+//    stage(name: 'Jira') {
+//        withEnv(['JIRA_SITE=LOCAL']) {
             jira_issues = parseTestResultXML resultsfilePath
-            if (jira_issues) {
-                // query jira & find if any duplicates, if dup, skip if not continue
-                jira_issues.each {
-                    issue ->
-                        def jiraIssue =
-                                [fields:
-                                         [project    : [id: '16941'],
-                                          summary    : issue.summary,
-                                          description: issue.details,
-                                          components : [[name: jiraComponent]],
-                                          fixVersions: [[name: fixVersions]],
-                                          issuetype  : [name: issueType]]]
-                        response = jiraNewIssue issue: jiraIssue
-                        echo response.successful.toString()
-                        echo response.data.toString()
-                        println issue.summary
-                        println issue.details
-                        println jiraComponent
-                        println jiraprojectName
-                        println issueType
-                        println fixVersions
-
-                }
-
-            }
-            else{
-                println "no issues found"
-            }
-        }
+//            if (jira_issues) {
+//                // query jira & find if any duplicates, if dup, skip if not continue
+//                jira_issues.each {
+//                    issue ->
+//                        def jiraIssue =
+//                                [fields:
+//                                         [project    : [id: '16941'],
+//                                          summary    : issue.summary,
+//                                          description: issue.details,
+//                                          components : [[name: jiraComponent]],
+//                                          fixVersions: [[name: fixVersions]],
+//                                          issuetype  : [name: issueType]]]
+//                        response = jiraNewIssue issue: jiraIssue
+//                        echo response.successful.toString()
+//                        echo response.data.toString()
+//                        println issue.summary
+//                        println issue.details
+//                        println jiraComponent
+//                        println jiraprojectName
+//                        println issueType
+//                        println fixVersions
+//
+//                }
+//
+//            }
+//            else{
+//                println "no issues found"
+//            }
+//        }
     }
-}
+//}
--- vars/parseTestResultXML.groovy
+++ vars/parseTestResultXML.groovy
@@ -1,20 +1,27 @@
 def call(String testresultsfilePath){
-    def jira_issues = []
+//    def jira_issues = []
     println testresultsfilePath
     try {
         def xml = new XmlParser().parse(testresultsfilePath)
         xml.testcase.each{
             test ->
-                def failedTests = [:]
+//                def failedTests = [:]
                 if (test.failure){
-                    failedTests.put('summary', test.@name)
-                    failedTests.put('file', test.@file)
-                    failedTests.put('details', test.failure.text())
-                    failedTests.put('description', test.properties.property.'@value'[0].trim())
-                    jira_issues << failedTests
+//                    failedTests.put('summary', test.@name)
+//                    failedTests.put('file', test.@file)
+//                    failedTests.put('details', test.failure.text())
+//                    failedTests.put('description', test.properties.property.'@value'[0].trim())
+//                    jira_issues << failedTests
+                    test.failure+ {Jira('https://jira.corporate.local/browse/ION-7935')}
                 }
         }
-        jira_issues
+
+        def writer = new FileWriter(testresultsfilePath)
+        //printer.preserveWhitespace = true
+        new XmlNodePrinter(new PrintWriter(writer)).print(xml)
+
+
+//        jira_issues
     }
     catch (FileNotFoundException exception){
         println 'file not found'
@@ -33,7 +40,7 @@ def call(String testresultsfilePath){
 //    }
 //    jira_issues
 
-    jira_issues
+    //jira_issues
 }
 
 //resultfilePath = '/Users/rpandey/Documents/my-git/hello_python/src/test/result.xml'
