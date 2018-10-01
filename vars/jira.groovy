e7343e2764eb17e70e155db8352f3c0a8c89f67e
Rajendra Pandey <rupandey@gmail.com>
2018-10-01 11:08:42 +0530

    adding escape logic

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -56,8 +56,8 @@ def jiraExists(issue){
     summary = issue.summary
     description = issue.details
 //    record = ((description.split('\\n')[-1]).split('/')[-1]).split(':')[0] // getting last line of stack trace with lineno.
-//    record = ((description.split('\\n')[-1]))
-    record = description.replaceAll( /([^a-zA-Z0-9])/, '\\\\$1' )
+    record = ((description.split('\\n')[-1]))
+    record = record.replaceAll( /([^a-zA-Z0-9])/, '\\\\$1' )
     echo summary
     echo description
     echo record
