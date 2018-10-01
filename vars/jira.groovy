1c40014b835b7d2d069a4341f0f42944bdbc3eb8
Rajendra Pandey <rupandey@gmail.com>
2018-10-01 10:13:30 +0530

    chekcking for issue in Jira

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -4,32 +4,70 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath,
         withEnv(['JIRA_SITE=LOCAL']) {
             jira_issues = parseTestResultXML resultsfilePath
             if (jira_issues) {
-                // query jira & find if any duplicates, if dup, skip if not continue
+
                 jira_issues.each {
                     issue ->
-                        def jiraIssue =
-                                [fields:
-                                         [project    : [id: '16941'],
-                                          summary    : issue.summary,
-                                          description: issue.details,
-                                          components : [[name: jiraComponent]],
-                                          fixVersions: [[name: fixVersions]],
-                                          issuetype  : [name: issueType]]]
-                        response = jiraNewIssue issue: jiraIssue
-                        echo response.successful.toString()
-                        echo response.data.toString()
-                        println issue.summary
-                        println issue.details
-                        println jiraComponent
-                        println jiraprojectName
-                        println issueType
-                        println fixVersions
+                        // query jira & find if any duplicates, if dup, skip if not continue
 
+                        bugExists = jiraExists(issue)
+                        if (bugExists) {
+                            echo 'not going to raise jira as, jira already exists'
+                            echo bugExists
+                        } else {
+//                            def jiraIssue =
+//                                    [fields:
+//                                             [project    : [id: '16941'],
+//                                              summary    : issue.summary,
+//                                              description: issue.details,
+//                                              components : [[name: jiraComponent]],
+//                                              fixVersions: [[name: fixVersions]],
+//                                              issuetype  : [name: issueType]]]
+//                            response = jiraNewIssue issue: jiraIssue
+//                            echo response.successful.toString()
+//                            echo response.data.toString()
+//                            println issue.summary
+//                            println issue.details
+//                            println jiraComponent
+//                            println jiraprojectName
+//                            println issueType
+//                            println fixVersions
+
+                            echo 'going to raise a jira'
+
+                        }
                 }
 
             }
             else{
-                println "no issues found"
+                echo 'There are no test failures..'
+            }
+        }
+    }
+}
+
+
+def jiraExists(issue){
+    summary = issue.summary
+    description = issue.details
+    record = description.split('\\n')[-1] // getting last line of stack trace with lineno.
+    echo summary
+    echo description
+    echo record
+
+    def jql_str = "PROJECT = IPF AND summary~${summary} AND description~${record}"
+    echo jql_str
+
+    node {
+        stage('JIRALoookup') {
+            withEnv(['JIRA_SITE=LOCAL']) {
+                def searchResults = jiraJqlSearch jql: jql_str
+                def jiraKeys = []
+                def issues = searchResults.data.issues
+                for (i = 0; i <issues.size(); i++) {
+                    echo issues[i].key
+                    jiraKeys<< issue[i].key
+                }
+                return jiraKeys
             }
         }
     }
--- vars/parseTestResultXML.groovy
+++ vars/parseTestResultXML.groovy
@@ -17,33 +17,8 @@ def call(String testresultsfilePath){
         jira_issues
     }
     catch (FileNotFoundException exception){
-        println 'file not found'
+        echo 'Exception:', exception
     }
-
-//    xml.testcase.each{
-//        test ->
-//            def failedTests = [:]
-//            if (test.failure){
-//                failedTests.put('summary', test.@name)
-//                failedTests.put('file', test.@file)
-//                failedTests.put('details', test.failure.text())
-//                failedTests.put('description', test.properties.property.'@value'[0].trim())
-//                jira_issues << failedTests
-//            }
-//    }
-//    jira_issues
-
     jira_issues
 }
 
-//resultfilePath = '/Users/rpandey/Documents/my-git/hello_python/src/test/result.xml'
-//
-//jira_issues = call(resultfilePath)
-//jira_issues.each{
-//    jira_issue ->
-//        println jira_issue.summary
-//        println jira_issue.file
-//        println jira_issue.details
-//        println jira_issue.description
-//}
-//
