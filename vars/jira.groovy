8005843809521529d64b28c23186c806e0d14ab6
Rajendra Pandey <rupandey@gmail.com>
2018-10-01 11:07:24 +0530

    adding escape logic

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -47,10 +47,17 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath,
 
 
 def jiraExists(issue){
+
+//    def inStr = 'src/test/test_helloworld.py:15: AssertionError'
+//    def outStr = inStr.replaceAll( /([^a-zA-Z0-9])/, '\\\\$1' )
+//    println "Output: $outStr"
+
+
     summary = issue.summary
     description = issue.details
 //    record = ((description.split('\\n')[-1]).split('/')[-1]).split(':')[0] // getting last line of stack trace with lineno.
-    record = ((description.split('\\n')[-1]))
+//    record = ((description.split('\\n')[-1]))
+    record = description.replaceAll( /([^a-zA-Z0-9])/, '\\\\$1' )
     echo summary
     echo description
     echo record
