18823ab51136dd2b04ef99efc7083c9b0f79afbc
Rajendra Pandey <rupandey@gmail.com>
2018-10-04 15:52:22 +0530

    fixing jira query

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,6 +1,6 @@
 def call(String jiraprojectName, String jiraComponent, String resultsfilePath, String logsPath,
          String issueType='Bug', String fixVersions='pipeline_fixes') {
-    stage(name: 'Create_Jira') {
+    stage(name: 'Jira') {
         try {
             withEnv(['JIRA_SITE=LOCAL']) {
                 jiraBaseUrl = getJiraBaseUrl()
@@ -11,12 +11,12 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath, S
                             def failedTest = [:]
                             def bugExists = []
                             if (test.failure) {
-                                failedTest.put('summary', test.@name)
-                                failedTest.put('file', test.@file)
-                                failedTest.put('details', test.failure.text())
-                                failedTest.put('description', test.properties.property.'@value'[0].trim())
+//                                failedTest.put('summary', test.@name)
+//                                failedTest.put('file', test.@file)
+//                                failedTest.put('details', test.failure.text())
+//                                failedTest.put('description', test.properties.property.'@value'[0].trim())
 
-                                bugExists = jiraExists(failedTest)
+                                bugExists = jiraExists(test, jiraComponent)
                                 if (bugExists) {
                                     echo 'Jira ticket already exists'
                                     bugExists.each {
@@ -33,10 +33,16 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath, S
                                 } else {
                                     echo 'Going to raise a Jira ticket'
                                     try{
+                                        summary = test.@name
+                                        message = test.failure.@'message'[0].split('   ')[0]
+                                        def jira_query = jiraComponent + ': ' + summary + ': ' + message
+                                        //jira_query = jira_query.replace("'", "\\'")
+                                        //jira_query =  "\"${jira_query}\""
+
                                         def jiraIssue =
                                                 [fields:
                                                          [project    : [idOrKey: jiraprojectName],
-                                                          summary    : failedTest.summary,
+                                                          summary    : jira_query,
                                                           description: failedTest.details,
                                                           components : [[name: jiraComponent]],
                                                           fixVersions: [[name: fixVersions]],
@@ -74,14 +80,23 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath, S
 }
 
 
-def jiraExists(issue){
-    summary = issue.summary
-    description = issue.details
-    description = ((description.split('\\n')[-1]))
-    description = (description.split('\\n')[-1]).replace('/', '\\u002f').split(' ')[0]
+def jiraExists(failedTest, jiraComponent){
+//    summary = issue.summary
+//    description = issue.details
+//    description = ((description.split('\\n')[-1]))
+//    description = (description.split('\\n')[-1]).replace('/', '\\u002f').split(' ')[0]
+//
+//    def jql_str = "summary~${summary} AND description~${description} AND status != Done"
+//    echo jql_str
 
-    def jql_str = "summary~${summary} AND description~${description} AND status != Done"
-    echo jql_str
+
+    summary = test.@name
+    message = test.failure.@'message'[0].split('   ')[0]
+    def jira_query = jiraComponent + ': ' + summary + ': ' + message
+    jira_query = jira_query.replace("'", "\\'")
+    jira_query =  "\"${jira_query}\""
+    def jql_str = "summary~${jira_query} AND status != Done"
+
     try{
         withEnv(['JIRA_SITE=LOCAL']) {
             try{
@@ -89,7 +104,18 @@ def jiraExists(issue){
                 def jiraKeys = []
                 def issues = searchResults.data.issues
                 for (i = 0; i <issues.size(); i++) {
-                    jiraKeys<< issues[i].key
+                    try{
+                        def issue = jiraGetIssue idOrKey: issues[i].key
+                        def bugSummary = issue.data.fields.summary
+                        println bugSummary
+                        if (jira_query == bugSummary){
+                            echo 'Duplicate bug found..'
+                            jiraKeys<<issues[i].key
+                        }
+                    }
+                    catch (Exception ex){
+                        echo 'failed to get details from jiraGetIssue'
+                    }
                 }
                 return jiraKeys
             }
