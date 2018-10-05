3509ab254cd5874b001a45e068f9ea59761d3789
Rajendra Pandey <rupandey@gmail.com>
2018-10-05 13:20:05 +0530

    code clean up

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -8,25 +8,15 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath, S
                     def xml = new XmlParser().parse(resultsfilePath)
                     xml.testcase.each {
                         test ->
-//                            def failedTest = [:]
-//                            def bugExists = []
                             if (test.failure) {
-//                                failedTest.put('summary', test.@name)
-//                                failedTest.put('message', test.failure.@'message'[0].split('   ')[0])
-//                                failedTest.put('file', test.@file)
-//                                failedTest.put('details', test.failure.text())
-//                                failedTest.put('description', test.properties.property.'@value'[0].trim())
-
                                 echo 'going to call jiraExists'
                                 def bugExists = []
                                 bugExists = jiraExists jiraComponent, test
                                 if (bugExists) {
                                     echo 'Jira ticket already exists'
                                     bugExists.each {
-                                        jira ->
-                                            def bugId = jiraBaseUrl + '/browse/' + jira
-                                            println bugId
-                                            test.failure.@'message' = test.failure.@'message'[0] + '\n' + bugId
+                                        jiraKey ->
+                                            appendBugId(jiraBaseUrl, jiraKey, test)
                                     }
                                 } else {
                                     echo 'Going to raise a Jira ticket'
@@ -50,34 +40,11 @@ def call(String jiraprojectName, String jiraComponent, String resultsfilePath, S
 
 
 def jiraExists(jiraComponent, failedTest){
-//    summary = issue.summary
-//    description = issue.details
-//    description = ((description.split('\\n')[-1]))
-//    description = (description.split('\\n')[-1]).replace('/', '\\u002f').split(' ')[0]
-//
-//    def jql_str = "summary~${summary} AND description~${description} AND status != Done"
-//    echo jql_str
-
-//    failedTest.put('summary', test.@name)
-//    failedTest.put('message', test.failure.@'message'[0].split('   ')[0])
-//    failedTest.put('file', test.@file)
-//    failedTest.put('details', test.failure.text())
-//    failedTest.put('description', test.properties.property.'@value'[0].trim())
-
-    echo 'inside jira exists'
-//    summary = failedTest.@name
-//     message = failedTest.failure.@'message'[0].split('   ')[0]
-//    def jira_summary = jiraComponent + ': ' + summary + ': ' + message
-//    jira_summary= jira_summary.take(220)
-
     jira_summary = getJiraSummary(jiraComponent, failedTest)
-
     def jira_summary_updated = jira_summary.replace("'", "\\'")
-//    jira_query_updated.take(240)  // for safe
 
     jira_summary_updated =  "\"${jira_summary_updated}\""
     def jql_str = "summary~${jira_summary_updated} AND status != Done"
-    println jql_str
 
     try{
         withEnv(['JIRA_SITE=LOCAL']) {
@@ -89,8 +56,6 @@ def jiraExists(jiraComponent, failedTest){
                     try{
                         def issue = jiraGetIssue idOrKey: issues[i].key
                         def bugSummary = issue.data.fields.summary
-                        println bugSummary
-                        println jira_summary
                         if (jira_summary == bugSummary){
                             echo 'Duplicate bug found..'
                             jiraKeys<<issues[i].key
@@ -128,14 +93,9 @@ def getJiraBaseUrl(){
 }
 
 def raiseBug(jiraComponent, fixVersions, issueType, labels, jiraBaseUrl, test) {
-//    echo 'Going to raise a Jira ticket'
     try {
         withEnv(['JIRA_SITE=LOCAL']) {
             try {
-//                summary = test.@name
-//                message = test.failure.@'message'[0].split('   ')[0]
-//                def jira_query = jiraComponent + ': ' + summary + ': ' + message
-//                jira_query.take(220)
                 summary = getJiraSummary(jiraComponent, test)
                 description = test.failure.text()
 
@@ -150,10 +110,7 @@ def raiseBug(jiraComponent, fixVersions, issueType, labels, jiraBaseUrl, test) {
                                   labels     : labels]]
 
                 response = jiraNewIssue issue: jiraIssue
-                def jira = jiraBaseUrl + '/browse/' + response.data.key
-                println jira
-
-                test.failure.@'message' = test.failure.@'message' + ' - ' + jira
+                appendBugId(jiraBaseUrl, response.data.key, test)
 
             } catch (Exception ex) {
                 echo 'Faild to raise jira ticket'
@@ -170,4 +127,9 @@ def getJiraSummary(jiraComponent, failedTest){
     def jira_summary = jiraComponent + ': ' + summary + ': ' + message
     jira_summary= jira_summary.take(220)
     return  jira_summary
+}
+
+def appendBugId(jiraBaseUrl, jiraKey, test){
+    jiraLink = jiraBaseUrl + '/browse/' + jiraKey
+    test.failure.@'message' = test.failure.@'message'[0] + '\n' + jiraLink
 }
