65de1b864b3b1f48c1910c4cd0d987084e5153a3
Rajendra Pandey <rupandey@gmail.com>
2018-10-29 16:45:28 +0530

    changing to plus

--- vars/raiseJiraTickets.groovy
+++ vars/raiseJiraTickets.groovy
@@ -82,7 +82,7 @@ def raiseBug(String projectId, String jiraComponent, String issueType, String fi
     try {
         withEnv(['JIRA_SITE=jira.corporate.local']) {
             try {
-                defaultLabel = ['PipelineBug']
+                defaultLabel = 'PipelineBug'
                 labels.plus(defaultLabel)
                 summary = getJiraSummary(jiraComponent, testName)
                 //description = test.failure.text()
